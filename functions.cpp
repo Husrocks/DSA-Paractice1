f288c7e6317dc03395e08718c81f28839094d3f1
unknown <zurqulnan@gmail.com>
2025-08-15 04:55:51 -0700

    Updated functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+/*
+
 void printName(){
     cout << "Striver" << endl; // Print a greeting message
 }
@@ -12,9 +14,22 @@ void printNamewithparameters(string name , int age){
 
 int sum(int a, int b){ // Function to calculate the sum of two integers
     int c = a + b; // Calculate the sum of a and b
-    return c; // Return the sum of a and b
+    return c; // Return the sum of a and b*/
+
+
+    //min max
+
+int maxx(int num1, int num2){ // Function to find the maximum of two integers
+    if( num1 > num2) {
+        return num1; // Return num1 if it is greater than num2
+    }
+    else {
+        return num2; // Return num2 if it is greater than or equal to num1
+    }
 }
 int main(){
+    
+    /*
     printName();
     string name; // Call the function to print the name
     int age; // Declare a variable to hold the age
@@ -29,7 +44,20 @@ int main(){
     cout << "Enter two numbers: ";  
     cin >> a >> b; // Read two integers from user input
     int c = sum(a, b); // Call the sum function to calculate the sum of a and b
-    cout << "The sum is: " << c << endl; // Print the result of the sum 
+    cout << "The sum is: " << c << endl; // Print the result of the sum */
+
+    //min max
+    int num1, num2; // Declare two integer variables for max function
+    cout << "Enter two numbers to find the maximum: ";
+    cin >> num1 >> num2;
+    int maximum = maxx(num1, num2); // Call the max function to find the maximum of num1 and num2
+    cout << "The maximum is: " << maximum << endl; // Print the result of the maximum
+
+    //In-Built Functions
+    // min max
+
+
+
     
     return 0; // Return 0 to indicate successful execution
 }
